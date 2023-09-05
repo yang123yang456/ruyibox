2dc3c9e38612f577ad0a72e7c2f9a0a72534edde
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-09-06 04:29:59 +0800

    Update VodController.java

--- app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
+++ app/src/main/java/com/github/tvbox/osc/player/controller/VodController.java
@@ -342,9 +342,6 @@ public class VodController extends BaseController {
                 FastClickCheckUtil.check(view);
                 try {
                     int playerType = mPlayerConfig.getInt("pl");
-                    if(playerType == 0){
-                        playerType = 1;
-                    };
                     int defaultPos = 0;
                     ArrayList<Integer> players = PlayerHelper.getExistPlayerTypes();
                     ArrayList<Integer> renders = new ArrayList<>();
