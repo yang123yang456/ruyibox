788d0b8d05ce29c5b48f03d7ddd8fede4e0df568
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-09-10 20:20:10 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -342,7 +342,7 @@ public class ApiConfig {
                 sbData.setPlayerType(siteData.msg.get(i).playertype);//设置播放器
                 //sbData.setExt(siteData.msg.get(i).extend);
                 //sbData.setCategories(DefaultConfig.safeJsonStringList(null, "categories"));
-		    JsonObject sitejob =  new JsonObject;
+		    JsonObject sitejob =  new JsonObject();
 		    sitejob = siteData.msg.get(i).getAsJsonObject();
 
                 if(siteData.msg.get(i).type!=3){
