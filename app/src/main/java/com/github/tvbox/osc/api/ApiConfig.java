dee454a7d7a2cd6af2cf38043ac1d7b069bc3a24
zhuziwuming <79568723+zhuziwuming@users.noreply.github.com>
2023-09-11 08:15:19 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -342,7 +342,14 @@ public class ApiConfig {
                 sbData.setPlayerType(siteData.msg.get(i).playertype);//设置播放器
                 sbData.setExt(siteData.msg.get(i).extend);
 		    if(getType()!=3){
-                    sbData.setCategories(DefaultConfig.safeJsonStringList(initData, "extend"));
+                   String str = siteData.msg.get(i).extend;
+                        if(str!=""){
+                            ArrayList<String> cates = new ArrayList<>();//新建一个列表						    
+                            String[] genres = str.split(","); // 将字符串按逗号分割成数组    
+                            for (String genre : genres) {    
+                                cates.add(genre.trim()); // 去除每个元素的首尾空格并添加到列表中    
+                            }   
+                                sbData.setCategories(cates);
 				}
                 //sbData.setCategories(DefaultConfig.safeJsonStringList(null, "categories"));
 
